f19c38c6818c3032fd285b7126cc5fe77fa6d9b0
AnielaAbramowicz <115256716+AnielaAbramowicz@users.noreply.github.com>
2025-06-02 21:18:16 +0200

    Update game_state.py

--- game_state.py
+++ game_state.py
@@ -20,21 +20,16 @@ class GameState:
         self.dataset_folder = os.path.join(base_path, "dataset_CK", "ck_images")
         self.df = pd.read_csv(self.csv_path)
 
-
         self.num_questions = num_questions  
-
-
 
         self.full_df = pd.read_csv(self.csv_path).reset_index(drop=True)
         self.df = self.full_df.copy() 
         self.df["shown"] = False  # Reset shown status 
         self.df = self.df[self.df["shown"] == False].reset_index(drop=True)
         self.shown_filenames = []
-       # self.df = self.full_df.copy()
-
+      
         self.emotion_prob = {"Angry" : 1/6, "Disgust": 1/6, "Fear": 1/6, "Happy": 1/6, "Sad": 1/6,"Surprise": 1/6}
     
-
         self.currect_score = []
         self.current_respond_time = []
         self.currect_correctness = []
@@ -42,7 +37,6 @@ class GameState:
 
         self.manual_difficulty = None  # “easy”, “medium”, “hard”, or None
         #self.manual_override_rounds = 0  # how many rounds we pause adaptation
-
 
         self.current_index = len(self.df) // 2
         self.score = 0
@@ -69,17 +63,12 @@ class GameState:
         return row 
 
 
-
-
     def set_difficulty(self, level, manual= False):
         self.current_index = 0 
              
         self.difficulty = level
-
         df_full = self.full_df.copy()
-
         df_full = df_full[~df_full["filename"].isin(self.shown_filenames)].reset_index(drop=True)
-
 
         length = len(df_full)
 
